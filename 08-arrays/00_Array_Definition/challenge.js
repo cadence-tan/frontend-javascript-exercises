f063c95ea4d0f1fc24eb139247dab7595c0dea53
Cadence <cadence.tan12@gmail.com>
2015-01-25 01:40:42 +0000

    first commit

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,9 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(num1,num2,num3,num4 ){
+							return [num1,num2,num3,num4];
 
-module.exports.firstAndLast = undefined;
+}
+
+module.exports.firstAndLast = function(array ){
+							return [array[0], array[array.length-1]];
+
+}                   
