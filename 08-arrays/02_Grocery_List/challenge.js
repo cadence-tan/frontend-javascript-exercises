352de6a3f735c4ea9aa3b74949d413e710879171
Cadence <cadence.tan12@gmail.com>
2015-01-26 05:24:58 +0000

    first commit 02_Grocery_List

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,13 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, itemList){
+		
+  if(itemList.toString().indexOf(item) == -1){   
+    itemList.push(item);    
+  }
+     return itemList;
+}
 
-module.exports.reverseSortedList = undefined;
+
+module.exports.reverseSortedList = function(list){
+
+	return list.sort().reverse();
+}
