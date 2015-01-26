0533a6372d38eb434f2935e8f72543dec94f367e
Cadence <cadence.tan12@gmail.com>
2015-01-26 21:43:20 +0000

    second commit 07-if-statements

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,39 +1,34 @@
-module.exports.favoriteNumber = favoriteNumber = function(fav, guess){
-								if(guess > fav){
-									return "Too high";
-
-								}else if(guess < fav){
-									return "Too low";
-
-								}else if(guess == fav){
-									return "You got it!";
-                				}
+module.exports.favoriteNumber = function(fav, guess){
+	if(guess > fav){
+	return "Too high";
+	}else if(guess < fav){
+	return "Too low";
+	}else if(guess == fav){
+	return "You got it!";
+	}
 }
 
-module.exports.checkLock = function(num1, num2, num3, num4){
-		if((num1 ===3 || num1 ===5 || num1 ===7) && (num2 ===2) && (num3 >= 5 && num3 <=100 ) && (num4 < 9 || num4 > 20 )){
-			return "correct";			
-		}else{
-			return "incorrect";
-        }
-}
+module.exports.checkLock = function(a, b, c, d) {
+  if ((a === 3 || a === 5 || a === 7) && (b === 2) && (c >= 5 && c <= 100) && (d < 9 || d > 20)) {
+    return 'correct';
+  } else {
+    return 'incorrect';
+  }
+};
 
 
-module.exports.canIGet = function(item, money){
-  
-						  	if(item === 'MacBook Air' && money  === 999){
-								return true;
+module.exports.canIGet = function(item, money) {
+  if (money >= 1 && item == 'Apple Sticker') {
+    return true;
+  } else if (money >= 999 && item == 'MacBook Air') {
+    return true;
+  } else if (money >= 1299 && item == 'MacBook Pro') {
+    return true;
+  } else if (money >= 2499 && item == 'Mac Pro') {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-							}else if(item === 'MacBook Pro' && money  === 1299){
-								return true;
 
-							}else if(item === 'Mac Pro' && money  === 2499){
-								return true;
-						    
-							}else if(item === 'Apple Sticker' && money  === 1){
-								return true;
-							
-						  	}else{
-						    	return false;
-						  }							
-}
