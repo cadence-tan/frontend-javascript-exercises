bae127a1ca5d470cacf3dccaf3e360864bba4050
Cadence <cadence.tan12@gmail.com>
2015-01-27 23:22:43 +0000

    first commit 02-object-operations

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,26 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object){
+  var newObject = {};
+  for(var property in object){
+     newObject[property] = object[property];   
+  }
+   return newObject;
+}
 
-module.exports.extend = undefined;
 
-module.exports.hasElems = undefined;
+module.exports.extend = function(dest, src){  
+  for(var property in src){
+    dest[property] = src[property];
+  }
+	return dest;
+};
+
+module.exports.hasElems = function(object, array){   
+	for(var i=0; i<array.length;){
+		if(object.hasOwnProperty(array[i])){
+          i++;
+        }else{
+          return false; 
+        }
+      }
+   return true;
+}
