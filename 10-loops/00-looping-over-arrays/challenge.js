cdad050e85c1ee5647244b3e4b5e620d2715e6a9
Cadence <cadence.tan12@gmail.com>
2015-01-27 04:38:30 +0000

    first commit 00-looping-over-arrays

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,26 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
 
-module.exports.splitAndLowerCaseString = undefined;
+	for(var i=0, sum=0; i< array.length; i++){
+		sum += array[i];
+}
+	return sum;
+};
 
-module.exports.addIndex = undefined;
+
+module.exports.splitAndLowerCaseString = function(inputString){
+
+	return inputString.toLowerCase().split(",");
+ };
+
+
+module.exports.addIndex = function(input){
+  
+  var string="", arrayOfStrings=[];
+  
+  for(var i=0; i<input.length; i++){
+    string = i + " is " + input[i];
+    arrayOfStrings.push(string);
+  }
+  return arrayOfStrings;
+}
+
